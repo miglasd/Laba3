2faa0db448cc6b02539b0d2cdd0d25b1fefbc638
miglasd <61839895+miglasd@users.noreply.github.com>
2020-05-12 19:13:24 +0300

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] textMass;
+            int count=0;
+            Console.WriteLine("Лабораторна робота №3 Варiант 5");
+            Console.WriteLine("Введiть текст:");
+            string text = Console.ReadLine();
+            textMass = text.Split(' ');
+            
+        foreach(var s in textMass)
+            {
+                if (s.Substring(0,1)=="а" || s.Substring(0, 1) == "о" || s.Substring(0, 1) == "у" || s.Substring(0, 1) == "і" || s.Substring(0, 1) == "е" || s.Substring(0, 1) == "и" || s.Substring(0, 1) == "А" || s.Substring(0, 1) == "О" || s.Substring(0, 1) == "У" || s.Substring(0, 1) == "І" || s.Substring(0, 1) == "Е" || s.Substring(0, 1) == "И")
+                count++;
+            }
+            Console.WriteLine("Слiв починаючих з голосной - "+count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторна робота №3 Варiант 5");
+            string path = File.ReadAllText( @"C:\Users\Dimon4ik\source\repos\ConsoleApp6\app.txt");
+            string[] massText = path.Split(" ");
+            foreach (var s in massText)
+            {
+                if (s.Substring(0, 1) == "s") Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
